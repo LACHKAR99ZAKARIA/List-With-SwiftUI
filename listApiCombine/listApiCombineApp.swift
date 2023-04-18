624f6f2959e171cd265a4d767204cd8da2c36771
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-04-18 13:50:02 +0000

    Initial Commit

---
+++ listApiCombine/listApiCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  listApiCombineApp.swift
+//  listApiCombine
+//
+//  Created by zakaria lachkar on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct listApiCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
